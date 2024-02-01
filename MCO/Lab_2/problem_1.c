1e132e7df315ac0b95165cb27243e68d42c276e5
arpithanr <71539085+arpithanr@users.noreply.github.com>
2024-02-01 07:54:33 +0000

    lab_2

---
+++ MCO/Lab_2/problem_1.c
@@ -0,0 +1,7 @@
+/*(i) Write a C program to search for a given element in a matrix. The program should take a matrix and a target
+element as input, and it should output whether the element is present in the matrix or not.
+
+(ii) Modify your code so that you convert the code into a function called search().Signature of search() can be as below:
+int search(int mat[][], int rows, int cols, int key).Let the search function return 0 on finding the key,
+and return -1 if the key is not present in the matrix. Write a suitable main function which invokes
+search() and observe your results. */
